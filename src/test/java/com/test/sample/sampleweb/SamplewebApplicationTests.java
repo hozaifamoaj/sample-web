1e58517b01ff3fc603f709dc158123ea315a91ba
hozaifa <hozaifa@istlbd.com>
2021-04-18 10:57:38 +0600

    first commit

---
+++ src/test/java/com/test/sample/sampleweb/SamplewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.sample.sampleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
